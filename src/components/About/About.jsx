e17092bba0dab42800ed2dec0f6b92f936d914a3
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-04-16 13:04:20 +0530

    Navbar Hero and About sections made responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,17 +3,19 @@ import "./About.css";
 const About = () => {
   return (
     <div className="about" id="about">
-    <h1>Insert an Image here</h1>
-      <h1>Something little About me</h1>
-      <p>
-        "Hey, I'm Ravindu Jayaweera, a web enthusiast with 2 years of experience
-        in the digital realm. I've had the pleasure of crafting over 40 websites
-        for clients from diverse corners of the globe. From sleek and stylish
-        portfolios to robust e-commerce platforms, I've honed my skills to
-        deliver top-notch web solutions. My passion lies in creating engaging
-        online experiences that captivate and convert. Let's team up and bring
-        your vision to life on the web!"
-      </p>
+      <h1 className="image">Insert an Image here</h1>
+      <div className="wrapper">
+        <h1>Something little About me</h1>
+        <p>
+          "Hey, I'm Ravindu Jayaweera, a web enthusiast with 2 years of
+          experience in the digital realm. I've had the pleasure of crafting
+          over 40 websites for clients from diverse corners of the globe. From
+          sleek and stylish portfolios to robust e-commerce platforms, I've
+          honed my skills to deliver top-notch web solutions. My passion lies in
+          creating engaging online experiences that captivate and convert. Let's
+          team up and bring your vision to life on the web!"
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,11 +16,32 @@ const Hero = () => {
               I'm <span className="name">Ravindu Jayaweera</span>
             </h1>
             <h2>Freelance Web Developer</h2>
+            <button className="hire">Hire Me</button>
             <button className="cv">Take a Look on my CV</button>
           </div>
           <div className="right">
             <h1>Image Apprears Here</h1>
           </div>
+          <h4>Scroll Down to Explore about Me!</h4>
+        </div>
+        <div className="socials">
+          <ul>
+            <li>
+              <h4>-</h4>
+            </li>
+            <li>
+              <a href="#">Github</a>
+            </li>
+            <li>
+              <a href="#">Linkedin</a>
+            </li>
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <h4>-</h4>
+            </li>
+          </ul>
         </div>
       </div>
     </motion.div>
