fcbd469168cde76289ae87d63b5e30a61466a62c
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-04-22 11:39:02 +0530

    Contact form functionality implemented using email.js

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,44 @@
 import "./Contact.css";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from 'sweetalert2';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_a6tbnxh", "template_7td2nfd", form.current, {
+        publicKey: "d2HZaUec7we8Nd3GA",
+      })
+      .then(
+        () => {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Thank you for Submitting your Requirements!",
+            showConfirmButton: false,
+            timer: 1500
+          });
+          form.current.reset();
+        },
+        (error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+          });
+        }
+      );
+  };
+
   return (
     <div id="contact">
       <h1 className="contact-title">Let Me know your Requirement</h1>
       <div className="contact-wrapper">
-        <form action="">
+        <form ref={form} onSubmit={sendEmail} id="contact-from">
           <div className="form-group">
             <label htmlFor="firstname">First Name:</label>
             <input type="text" name="firstname" id="firstname" />
